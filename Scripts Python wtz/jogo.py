450eee07f430020352b09cb233f1d449a4536490
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2021-06-04 00:39:44 -0300

    modificando

--- Scripts Python wtz/jogo.py
+++ Scripts Python wtz/jogo.py
@@ -1,10 +1,1 @@
-b = 'sim'
-a = input('o emerson chupou hoje?')
-if a == b :
-    print("emerson cavalo chupador")
-else:
-        a = input('ele gostaria de chupar?')
-        if a == b:
-            print('emerson cavalo chupador de piroca')
-            
-        
+
