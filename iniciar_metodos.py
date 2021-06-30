35edf1f87e60a5e8da1285cafe6afed3a2602250
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2021-06-30 04:21:16 -0300

    Inicio

--- iniciar_metodos.py
+++ iniciar_metodos.py
@@ -1,17 +1,23 @@
 
 import numpy as np
 import math
-from sympy import *
-a,b = Symbol('x y')
-init_printing(use_unicode=True)
-import metodosnumericos.metodosrefinamento.bissecao
+import sympy
+import metodosnumericos.localizacao.teorema_bolzano as teorema
 # from metodosnumericos.metodosrefinamento.bissecao import pm
+#from sympy import symbols
+#from sympy.core.function import diff
+#from sympy.interactive.printing import init_printing
+
 
 print("\nInforme os dados necessarios para realizacao dos metodos iterativos, sendo ele a funcao, intervalo [a,b] e sua precisao.")
-print("\nExemplo: \n(2*ln(x**2))/sqrt(4)\na:3\nb:5\np:0,0005\n\n")
-f = input("Digite a sua funcao: ")
+print("\nExemplo:" , "(2*ln(x**2))/sqrt(4)", "a:3", "b:5", "precisao: 0.0005\n", sep="\n")
+f = sympy.sympify(input("Digite a sua funcao: "))
 a = int(input("Digite a: "))
 b = int(input("Digite b: "))
-xn= metodosnumericos.metodosrefinamento.bissecao.pm(a,b)
-print(xn)
-diff(a**2)
+precisao = float(input("Digite precisao: "))
+if(teorema.bolzano(a,b,f) == False):
+    print("Nao e possivel afirma a existencia de uma solucao no intervalo [{},{}]".format(a,b))
+else:
+    print("Intervalo contem pelo menos uma solucao, pois f({})*f({})<0!".format(a,b))
+    print("{}".format(precisao))
+#print(sympy.diff(f))
--- metodosnumericos/localizacao/teorema_bolzano.py
+++ metodosnumericos/localizacao/teorema_bolzano.py
@@ -1,5 +1,8 @@
+import numpy as np
 import math
-
+import sympy
+x,c = sympy.symbols('x c')
+sympy.init_printing(use_unicode=True)
 
 # Seja f(x) continua no intervalo [a,b] e f(a).f(b) < 0 , então existe pelo menos um ponto em que x pertencente ao intervalo (a,b) tal que f(x) = 0.
 
@@ -10,6 +13,18 @@ import math
 #                                           x -> a+
 #   f deve ser continua a esquerda de b.    lim f(x) = f(b)
 #                                           x -> b-
+
 def bolzano(a,b,f):
-    f
+    f = sympy.Lambda(x,sympy.sympify(f))
+#    print("\n f(c)=",f(c))
+#    print("\n limit(f(x),x,c)=", sympy.limit(f(x), x, c))
+    if((sympy.limit(f(x), x, c) == f(c)) & (sympy.limit(f(x), x, a) == f(a)) & (sympy.limit(f(x), x, b) == f(b))):
+        if(f(a)*f(b) < 0):
+            return True
+        else:
+            return False
+    else:
+        print("A funcao nao pode ser considera continua no intervalo dado, por esse motivo nao pode-se aplicar o Teorema de Bolzano")
+        return False
+
 
