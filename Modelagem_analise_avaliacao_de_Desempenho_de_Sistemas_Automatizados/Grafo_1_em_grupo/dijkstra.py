4c1b31046be1ba5f0fcbb5a9c3fad76a77cfed02
wagner rodrigues <wagner.rosa@grad.ufsc.br>
2023-10-02 22:39:36 -0300

    fix: algoritmo Dijkstra

--- Modelagem_analise_avaliacao_de_Desempenho_de_Sistemas_Automatizados/Grafo_1_em_grupo/dijkstra.py
+++ Modelagem_analise_avaliacao_de_Desempenho_de_Sistemas_Automatizados/Grafo_1_em_grupo/dijkstra.py
@@ -1,53 +1,65 @@
-def dijkstra(matriz, vertice_inicial):
-    n = len(matriz)
-    caminho_mais_curto = [float('inf')] * n
-    caminho_mais_curto[vertice_inicial] = 0
-    visitados = [False] * n
-    caminhos = {}
-    u = vertice_inicial
+class Grafo:
+    def __init__(self, matriz):
+        self.matriz = matriz
 
-    while True:
-        distancia_minima = float('inf')
-        visitados[u] = True
-
-        for k in range(n):
-            if not visitados[k] and matriz[u][k] and caminho_mais_curto[u] + matriz[u][k] < caminho_mais_curto[k]:
-                caminho_mais_curto[k] = caminho_mais_curto[u] + matriz[u][k]
-                caminhos[(u+1, k+1)] = matriz[u][k]
-
-        u_next = -1
-        for i in range(n):
-            if not visitados[i] and caminho_mais_curto[i] < distancia_minima:
-                u_next = i
-                distancia_minima = caminho_mais_curto[i]
-
-        if u_next == -1:
-            break
+    def calcular_caminhos(self, inicio):
+        n = len(self.matriz)
+        # Lista para armazenar a menor distância de 'inicio' para cada vértice
+        menor_caminho = [float('inf')] * n
+        # Lista para armazenar o vértice anterior no caminho mínimo
+        antecessor = [-1] * n
+        # Lista para marcar vértices já visitados
+        visitados = [False] * n
+        menor_caminho[inicio] = 0  # A distância do vértice inicial para ele mesmo é 0
         
-        u = u_next
-
-    return caminhos
-
+        for _ in range(n):
+            # Encontrar o vértice com a menor distância dentre os não visitados
+            minimo = float('inf')
+            u = -1
+            for i in range(n):
+                if not visitados[i] and menor_caminho[i] < minimo:
+                    minimo = menor_caminho[i]
+                    u = i
+            visitados[u] = True  # Marcar o vértice 'u' como visitado
+            
+            # Atualizar as distâncias dos vértices adjacentes a 'u'
+            for v in range(n):
+                if self.matriz[u][v] and not visitados[v] and menor_caminho[u] + self.matriz[u][v] < menor_caminho[v]:
+                    menor_caminho[v] = menor_caminho[u] + self.matriz[u][v]
+                    antecessor[v] = u
+        
+        # Construir os caminhos a partir da lista de antecessores
+        caminhos = {}
+        for i in range(n):
+            caminho = []
+            atual = i
+            while atual != -1:
+                caminho.append(str(atual + 1))  # Convertendo índices para números, começando de 1
+                atual = antecessor[atual]
+            caminho.reverse() # Inverter a lista para que o caminho comece em 'inicio'
+            caminhos[i + 1] = ' -> '.join(caminho)  
+        
+        return caminhos
 
 
 
 
 matriz_grafo = [
     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 0
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 1
-    [0, 0, 0, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 2
-    [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 3
-    [0, 0, 3, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 4
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 5
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 1
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 2
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 3
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 4
+    [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 5
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 6
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 7
-    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], # 8
-    [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0], # 9
-    [0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #10
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], #11
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #12
-    [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #13
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #14
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 7
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], # 8
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], # 9
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #10
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], #11
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #12
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #13
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #14
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #15
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #16
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #17
@@ -64,5 +76,6 @@ matriz_grafo = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #28 
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #29
 ]
-vertice_inicial = 0  # Começando do vértice 0
-print(dijkstra(matriz_grafo, vertice_inicial))
+# Executando o algoritmo de Dijkstra iterativamente
+caminho = Grafo(matriz_grafo)
+print(caminho.calcular_caminhos(0))
